65770d3dd344fe5be0cfb8883b7397307b011063
siyingcheng <siyingcheng@126.com>
2021-02-20 01:00:09 +0800

    save

--- simples/user/blueprint.py
+++ simples/user/blueprint.py
@@ -6,9 +6,11 @@
 # @DateTime :   2021/2/14 21:21
 # @Project  :   simples
 
-from flask import Blueprint, render_template, url_for
+from flask import Blueprint, render_template, url_for, redirect, get_flashed_messages
 
+from simples import db
 from simples.user.forms import UserForm
+from simples.user.models import Users
 
 user_bp = Blueprint('user', __name__)
 
@@ -23,7 +25,23 @@ def login():
     return render_template('user/login.html')
 
 
-@user_bp.route('/register')
+@user_bp.route('/register', methods=['GET', 'POST'])
 def register():
     form = UserForm()
+    if form.validate_on_submit():
+        username = form.data.get('username')
+        password = form.data.get('password')
+        email = form.data.get('email')
+        user = Users()
+        user.username = username
+        user.set_password(password)
+        user.email = email
+        db.session.add(user)
+        db.session.commit()
+        print('添加成功：' + username)
+        return redirect(url_for('user.index'))
+    else:
+        print('校验失败')
+        for msg in get_flashed_messages():
+            print('error->' + msg)
     return render_template('user/register.html', form=form)
--- simples/user/forms.py
+++ simples/user/forms.py
@@ -5,10 +5,13 @@
 # @Contact  :   siyingcheng@126.com
 # @DateTime :   2021/2/16 21:09
 # @Project  :   simples
+import re
+
+from flask import flash
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.fields.html5 import EmailField
-from wtforms.validators import DataRequired, Email, Length
+from wtforms.validators import DataRequired, Email, Length, ValidationError
 
 from simples.settings import INVITE_CODE
 
@@ -21,10 +24,34 @@ class UserForm(FlaskForm):
     code = StringField('Invite Code', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
+    def validate_username(self, field):
+        if not re.match('^[a-zA-Z0-9_]{6,32}$', field.data):
+            flash('请输入6到32位的大小写字母、数字或者下划线的组合')
+            raise ValidationError('请输入6到32位的大小写字母、数字或者下划线的组合')
+
+    def validate_password(self, field):
+        if not re.match(r'^[a-zA-Z0-9`~!@#$%^&*()-_=+\\|\[\]{}:;\'\"?/.>,<]{8,256}$', field.data):
+            flash('请输入8到256位的密码，必须包含大小、写字母、数字或者特殊符号的至少两种组合')
+            raise ValidationError('请输入8到256位的密码，必须包含大小、写字母、数字或者特殊符号的至少两种组合')
+        match_count = 0
+        if re.search(r'[0-9]', field.data):
+            match_count += 1
+        if re.search(r'[a-z]', field.data):
+            match_count += 1
+        if re.search(r'[A-Z]', field.data):
+            match_count += 1
+        if re.search(r'[`~!@#$%^&*()-_=+\\|\[\]{}:;\'\"?/.>,<]', field.data):
+            match_count += 1
+        if match_count < 2:
+            flash('请输入8到256位的密码，必须包含大小、写字母、数字或者特殊符号的至少两种组合')
+            raise ValidationError('请输入8到256位的密码，必须包含大小、写字母、数字或者特殊符号的至少两种组合')
+
     def validate_repassword(self, field):
-        if self.password != field.data:
-            raise ValueError('两次密码不一致')
+        if self.password.data != field.data:
+            flash('两次密码不一致')
+            raise ValidationError('两次密码不一致')
 
     def validate_code(self, field):
         if field.data != INVITE_CODE:
-            raise ValueError('邀请码错误')
+            flash('邀请码错误')
+            raise ValidationError('邀请码错误')
